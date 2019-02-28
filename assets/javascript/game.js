eccec8f33948a556c334a867075c459b77459351
Michael Anderson <MacTerrier0@michaels-mbp.attlocal.net>
2019-02-27 22:18:13 -0800

    updated objects and loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,16 @@
 // Initialize variables 
 let word = 'rabbit'
 let numGuesses = word.length
-let guesses = [1, 0 , 1, 0, 0]
+let guesses = [1, 0, 1, 0, 0]
 let guessesLeft = word.length - guesses.length
 let winTotal
 let lossTotal
+
+// create a constant to sum the 1's for the correct guesses
+const wins = ga => ga.reduce( (a,b) => a+b, 0 )
+// wins and losses 
+winTotal = wins(guesses)
+lossTotal = guesses.length - winTotal
 
 // Object to hold the information
 let showObj = {
@@ -15,20 +21,16 @@ let showObj = {
     wins: winTotal,
     losses: lossTotal
 }
-// create a constant to sum the 1's for the correct guesses
-const wins = ga => ga.reduce( (a,b) => a+b, 0 )
-
-// wins and losses 
-winTotal = wins(guesses)
-lossTotal = guesses.length - winTotal
 
 console.log(showObj);
 
-document.getElementById("update").innerHTML = showObj.word;
+document.getElementById("update").innerHTML = 'Wins: ' + showObj.wins;
 
-str = 'wwwww'
-for (let i = 0; i < str.length; i++) {
+
+let str = 'hotdog'
+
+for (let i =0; i < str.length; i++) {
     let thing = document.createElement('span')
-    thing.textContent = '-'
+    thing.textContent = ' _ ' // str[i]
     document.querySelector('#myDiv').append(thing)
 }
