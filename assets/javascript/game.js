b3e9ce6a8abd7ee31afb3f1c31264722a18d1a21
Michael Anderson <MacTerrier0@Michaels-MacBook-Pro.local>
2019-03-03 00:28:33 -0800

    improving the opject

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,64 @@
 // Initialize variables 
-let word = 'Anderson'
-let letter = []
+let word = 'anderson'
 const numGuesses = word.length
 let guesses = []
-let guessesLeft 
-let winTotal
-let lossTotal
+let guessesLeft
+let winTotal = []
+let lossTotal = []
+let showWord = []
 
 // create a constant to sum the 1's for the correct guesses
-const wins = ga => ga.reduce( (a,b) => a+b, 0 )
+const wins = ga => ga.reduce((a, b) => a + b, 0)
 
 // Object to hold the information
 let showObj = {
     word: word,
-    guessLen: numGuesses,
+    guessLen() {
+        // length of word
+        return this.word.length
+    },
     guessArr: guesses,
-    guessLeft: guessesLeft,
+    guessLeft() {
+        // guesses left
+        return this.guessLen() - this.guessArr.length
+    },
+    // showWordArr() {
+    //     for (let i = 0; i < this.guessLen(); i++) {
+    //         if (this.word.match(this.guessArr[i])) {
+    //             let thing = document.createElement('span')
+    //             document.querySelector('#myDiv').append(thing)
+    //             thing.textContent = this.guessArr[i]
+    //         } else {
+                
+    //         }
+    //     }
+    // },
     wins: winTotal,
     losses: lossTotal
 }
 
-document.onkeydown = ({ key }) => {
-    showObj.guessArr.push(key)
-    showObj.guessLeft = numGuesses - showObj.guessArr.length
-    document.getElementById("update").innerHTML = 'Guesses Left: ' + showObj.guessLeft;
-};
+document.getElementById("guessLeft").innerHTML = 'Guesses Left: ' + showObj.guessLeft();
+
+for (let i =0; i < showObj.guessLen(); i++) {
+    let thing = document.createElement('span')
+    thing.textContent = ' _ ' // str[i]
+    document.querySelector('#myDiv').append(thing)
+}
 
 // wins and losses 
 winTotal = wins(guesses)
 lossTotal = guesses.length - winTotal
 
+document.onkeydown = ({ keyCode, key }) => {
+    if (keyCode >= 65 && keyCode <= 90) {
+        // save the letters pressed
+        showObj.guessArr.push(key)
+        // showObj.showWordArr()
+    }
+    document.getElementById("guessLeft").innerHTML = 'Guesses Left: ' + showObj.guessLeft();
 
 
-let str = 'hotdog'
 
-for (let i =0; i < showObj.word.length; i++) {
-    let thing = document.createElement('span')
-    thing.textContent = ' _ ' // str[i]
-    document.querySelector('#myDiv').append(thing)
-}
+};
+
+
--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -2,9 +2,24 @@ var word2 = 'Hello World'
 var res = word2.split("")
 var lookup = word2.indexOf("l")
 
-// document.getElementById("myTest").innerHTML = res
 
-document.onkeydown = ({ key }) => {
-    document.getElementById("myTest").innerHTML = key
-    showObj.guessArr.push(key)
-  };
+let showObj = {
+  word: word,
+  guessLen: numGuesses,
+  guessArr: guesses,
+  guessArrLen = this.guessArr.length,
+  guessLeft () {
+    let guessLeft = this.guessLen - this.guessArrLen
+  },
+  wins: winTotal,
+  losses: lossTotal,
+
+}
+
+
+
+// // document.getElementById("myTest").innerHTML = res
+// document.onkeydown = ({ key }) => {
+//     document.getElementById("myTest").innerHTML = key
+//     showObj.guessArr.push(key)
+//   };
